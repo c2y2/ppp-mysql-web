23802e5b02bbd20a6bb0747459211c24ae942d9e
c2y2 <lexiangtaotao@gmail.com>
2014-03-01 22:46:49 +0800

    add init.sql

---
+++ db/init.sql
@@ -0,0 +1,23 @@
+
+/*创建数据库*/
+create database pptpd default character set utf8;
+
+use pptpd;
+
+/*管理员表*/
+create table pptpd_admin(
+id int(11) not null primary key auto_increment,
+username varchar(30) not null,
+password varchar(32) not null,
+memo varchar(100)
+);
+
+/*vpn用户表*/
+create table pptpd_user( 
+id int(11) not null primary key auto_increment, 
+username varchar(30) not null, 
+password varchar(32) not null, 
+status int(11) not null default '0', 
+clientip varchar(15) not null, 
+serverip varchar(15) not null
+);
